9d7e2c5b682e2a40c6f83ded8d5562286f85053e
neyona <40152041+neyona@users.noreply.github.com>
2019-09-26 12:17:10 -0400

    Update settings.py

--- ddrfr_boilerplate_project/settings.py
+++ ddrfr_boilerplate_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i5ax=jd0)44i!h23e$wx$j=773vd-g+e(24pyyxf+n%zi$n)p$'
+SECRET_KEY = '************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
